b38af49e6fbfb2bba44e01e2274b16d5d9a3f873
chen42333 <cch92423@gmail.com>
2024-06-09 16:11:03 +0800

    Add stream.sh and update README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Embedded-Systems-Capstone-Final-Project
+[Slide](https://docs.google.com/presentation/d/1Cs7orE9O9-VhAgoquzBV2LjvHrLx7AnTmYlM9DYbEMk/edit?usp=sharing)
---
+++ stream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+        echo "Usage: ./video.sh <session-key>"
+        exit
+fi
+
+raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$1
+
